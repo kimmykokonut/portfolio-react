a8b2ee9ccbc8fbed2bd414c0981735c960794280
Kim Robinson <kimmykokonut@gmail.com>
2024-01-05 12:20:40 -0800

    add content

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,32 @@
+import nps from './../assets/images/park.png';
+import praline from './../assets/images/praline.png';
+import game from './../assets/images/game.png';
+
 function Projects() {
-  return(
+  return (
     <>
-    <h2>Projects</h2>
+      <h2>Projects</h2>
+      <p>an evolving list of projects as I learn and add to my skillset</p>
+      <hr />
+      <h4>National Park Finder</h4>
+      <p>A pair project to explore API calls and MUI in React</p>
+      <a href="https://github.com/kimmykokonut/national-park-api">Github Link</a>
+      {/* to add: gh-pages */}
+      <img height="300" src={nps} alt="National Park Finder Application" />
+      <p> React | Typescript | Material UI | Vite</p>
+      <hr />
+      <h4>Inventory Tracker</h4>
+      <p>Independent project that tracks inventory with CRUD functionality</p>
+      <a href="https://kimmykokonut.github.io/inventory-tracker-cra-redux/">GitHub Pages Link</a>
+      <img height="300" src={praline} alt="Inventory tracker application" />
+      <p> React | Javascript | Bootstrap | Redux</p>
+<hr />
+      <h4>Not Wordle</h4>
+      <p>A pair project to learn React and refactor class to functional components and add Redux to manage state</p>
+      <a href="https://kimmykokonut.github.io/word-puzzle/">You can play the game!</a>
+      <img height="300" src={game} alt="Word Puzzle Game"/>
+      <p> React | Javascript | CSS | Redux</p>
+      <hr />
     </>
   )
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,57 +1,48 @@
 function Skills() {
   return (
     <>
-      <table>
-        <tr>
-          <td><h3>Technical Skills</h3></td>
-          <td><h3>Skills to come...</h3></td>
-          <td><h3>Soft Skills</h3></td>
-        </tr>
-        <tr>
-          
-        <td>
-          <ul>
-            <li>JavaScript</li>
-            <li>JSX</li>
-            <li>Typescript</li>
-            <li>Test Driven Development (TDD)</li>
-            <li>Git</li>
-            <li>React.js</li>
-            <li>Redux.js</li>
-            <li>Node.js</li>
-            <li>Webpack</li>
-            <li>Bootstrap</li>
-            <li>CSS</li>
-            <li>Html</li>
-            <li>Firebase and Firestore Database</li>
-              <li>Agile</li>
-              <li>Scrum</li>
-              <li>Pair Programming</li>
-              <li>API</li>
-          </ul>
-        </td>
-        
-        <td>
-          <ul>
-            <li>C#</li>
-            <li>.NET Framework</li>
-          </ul>
-        </td>
-        
-        <td>
-          <ul>
-            <li>Communication</li>
-            <li>Problem Solving</li>
-            <li>Adaptable</li>
-            <li>Time Management</li>
-            <li>Attention to detail</li>
-            <li>Empathetic</li>
-            <li>Team collaborator</li>
-            <li>Continuous learner</li>
-          </ul>
-        </td>
-        </tr>
-      </table>
+      <div>
+        <h3>Technical Skills</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>JSX</li>
+          <li>Typescript</li>
+          <li>Test Driven Development (TDD)</li>
+          <li>Git</li>
+          <li>React.js</li>
+          <li>Redux.js</li>
+          <li>Node.js</li>
+          <li>Webpack</li>
+          <li>Bootstrap</li>
+          <li>CSS</li>
+          <li>Html</li>
+          <li>Firebase and Firestore Database</li>
+          <li>Agile</li>
+          <li>Scrum</li>
+          <li>Pair Programming</li>
+          <li>API</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Skills to come...</h3>
+        <ul>
+          <li>C#</li>
+          <li>.NET Framework</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Soft Skills</h3>
+        <ul>
+          <li>Communication</li>
+          <li>Problem Solving</li>
+          <li>Adaptable</li>
+          <li>Time Management</li>
+          <li>Attention to detail</li>
+          <li>Empathetic</li>
+          <li>Team collaborator</li>
+          <li>Continuous learner</li>
+        </ul>
+      </div>
     </>
   )
 }
